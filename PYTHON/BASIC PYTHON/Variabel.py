72b5d4fb26d01e9b8bc16497e965fc157fe5482b
Bakti Aji <58435662+baktiajib@users.noreply.github.com>
2024-06-03 08:55:10 +0700

    Create Variabel.py

---
+++ PYTHON/BASIC PYTHON/Variabel.py
@@ -0,0 +1,21 @@
+# Variabel adalah tempat menyimpan data
+
+# Inisialisasi dan penugasan nilai
+a = 123  # menyimpan nilai 123 ke variabel a
+b = 234  # menyimpan nilai 234 ke variabel b
+x = a + b  # menyimpan hasil penjumlahan a dan b ke variabel x
+
+# Penamaan variabel yang baik
+nilai_x = 80  # menyimpan nilai 80 ke variabel nilai_x
+juta2 = 2000000  # menyimpan nilai 2 juta ke variabel juta2
+
+# Pemanggilan dan pencetakan variabel x (1)
+print('nilai x =', x)  # mencetak nilai x
+
+# Operasi pengurangan pada variabel x dan pencetakan (2)
+x -= 300  # mengurangi nilai x sebesar 300
+print('nilai x =', x)  # mencetak nilai x yang baru
+
+# Penugasan tidak langsung (indirect) dan pencetakan variabel z
+z = x  # menyimpan nilai x ke variabel z
+print('nilai z =', z)  # mencetak nilai z

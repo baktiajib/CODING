ea2880b4c33b82824d5363ea4d9598752e9a47aa
Bakti Aji <58435662+baktiajib@users.noreply.github.com>
2024-06-03 09:08:00 +0700

    Create 6. Operasi Aritmatika.py

---
+++ PYTHON/BASIC PYTHON/6. Operasi Aritmatika.py
@@ -0,0 +1,31 @@
+# Definisikan dua angka untuk operasi aritmatika
+a = 15
+b = 4
+
+# Penjumlahan
+penjumlahan = a + b
+print(f'Hasil penjumlahan {a} + {b} = {penjumlahan}')
+
+# Pengurangan
+pengurangan = a - b
+print(f'Hasil pengurangan {a} - {b} = {pengurangan}')
+
+# Perkalian
+perkalian = a * b
+print(f'Hasil perkalian {a} * {b} = {perkalian}')
+
+# Pembagian
+pembagian = a / b
+print(f'Hasil pembagian {a} / {b} = {pembagian}')
+
+# Pembagian bulat (floor division)
+pembagian_bulat = a // b
+print(f'Hasil pembagian bulat {a} // {b} = {pembagian_bulat}')
+
+# Modulus (sisa bagi)
+modulus = a % b
+print(f'Sisa bagi {a} % {b} = {modulus}')
+
+# Eksponensial (pangkat)
+eksponensial = a ** b
+print(f'Hasil eksponensial {a} ** {b} = {eksponensial}')
